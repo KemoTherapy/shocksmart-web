60caf6c1f37b6928b74204e4434e8a44d9466a78
K Kamaleldin <128553298+KemoTherapy@users.noreply.github.com>
2026-05-19 19:06:44 -0400

    video add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -166,7 +166,7 @@ function Landing({ onLaunch, onDocs }) {
   <iframe
     width="100%"
     height="100%"
-    src="https://www.youtube.com/embed/T_Z0_myEfx8"
+    src="https://www.youtube.com/embed/T_ZO_myEfx8"
     title="YouTube video player"
     frameBorder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
